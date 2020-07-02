6c8e469cb52484199dc4f1ab3db12400cdbb19ae
kyoolbaba <kyoolbaba@gmail.com>
2020-07-02 09:09:30 +0530

    Added getmapping to get list og users in the list

--- src/main/java/com/springrestexercise/demo/controller/UserController.java
+++ src/main/java/com/springrestexercise/demo/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.springrestexercise.demo.controller;
 
+import com.springrestexercise.demo.entity.User;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 @RestController
-@RequestMapping("user")
+@RequestMapping("/rest")
 public class UserController {
 
 
-    @GetMapping
-    public String getUser(){
-        return "get user";
+
+    @GetMapping("/users")
+    public List<User> getUser(){
+        List<User> users = new ArrayList<>();
+        users.add(new User("milan","gowda","milano","milan@gmail.cm","123456"));
+        users.add(new User("rahul","gowda","rahulo","rahul@gmail.cm","784484"));
+        users.add(new User("nikhil","gowda","nikhilno","nikhi@gmail.cm","123456"));
+        users.add(new User("vishwa","kumar","vishwo","viahwa@gmail.cm","123456"));
+        return users ;
     }
 
     @PostMapping
